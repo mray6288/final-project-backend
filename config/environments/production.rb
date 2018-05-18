4dfb83a92b955b74be5b1859af36d00f6f904d88
Ray Blasko <mray6288@gmail.com>
2018-05-18 12:25:13 -0400

    config public_file in config/environemtns/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,13 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {
+    'Access-Control-Allow-Origin' => '*',  # only for debugging
+    # 'Access-Control-Allow-Origin' => 'https://xxxxxxxxxxxxxx.cloudfront.net',
+    'Access-Control-Request-Method' => %w{GET OPTIONS}.join(",")
+  }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
